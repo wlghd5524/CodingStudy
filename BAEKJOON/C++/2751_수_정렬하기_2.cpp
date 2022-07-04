823bc57d4aa250f72bdd2973a673a81bb7583a82
wlghd5524 <wlghd5524@gmail.com>
2022-07-04 10:46:00 +0900

    Silver 5

---
+++ BAEKJOON/C++/2751_수_정렬하기_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr,arr+n);
+    for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
+        cout << arr[i] << "\n";
+    return 0;
+}
