4c06710e781827c077ca654ab9c6a4d37bb5a7b6
wlghd5524 <wlghd5524@gmail.com>
2022-07-10 23:29:14 +0900

    Silver 5

--- BAEKJOON/C++/1676_팩토리얼_0의개수.cpp
+++ BAEKJOON/C++/1676_팩토리얼_0의개수.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main() {
     int n, count = 0;
     cin >> n;
-    for(int i = 2;i<=n;i++) {
-        if(i%5==0) 
+    for (int i = 2; i <= n; i++) {
+        if (i % 5 == 0)
             count++;
-        if(i%25==0)
+        if (i % 25 == 0)
             count++;
-        if(i%125==0) 
+        if (i % 125 == 0)
             count++;
     }
     cout << count << "\n";
