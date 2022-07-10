38ffa9bbebfe4db02278f906fe4e637a0c39d0e7
wlghd5524 <wlghd5524@gmail.com>
2022-07-10 23:28:38 +0900

    Silver 5

--- BAEKJOON/C++/2161_카드1.cpp
+++ BAEKJOON/C++/2161_카드1.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 int main() {
     int n;
     cin >> n;
     vector<int> arr(n);
-    for(int i=0;i<n;i++) {
-        arr[i] = i+1;
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
     }
-    while(true) {
-        if(arr.size()==1) {
+    while (true) {
+        if (arr.size() == 1) {
             cout << arr[0];
             break;
         }
