e401489a8c9dd5952900ff163519376163b30a02
wlghd5524 <wlghd5524@gmail.com>
2022-07-12 16:36:44 +0900

    Silver 5

--- BAEKJOON/C++/17478_재귀함수가뭔가요.cpp
+++ BAEKJOON/C++/17478_재귀함수가뭔가요.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int cnt = 0;
 void underBar() {
-    for(int i=0;i<cnt*4;i++) 
+    for (int i = 0; i < cnt * 4; i++)
         cout << "_";
 }
 void func(int n) {
     underBar();
     cout << "\"재귀함수가 뭔가요?\"\n";
-    if(n==0) {
+    if (n == 0) {
         underBar();
         cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
         return;
@@ -20,7 +20,7 @@ void func(int n) {
     underBar();
     cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
     cnt++;
-    func(n-1);
+    func(n - 1);
     underBar();
     cout << "라고 답변하였지.\n";
     cnt--;
@@ -32,4 +32,5 @@ int main() {
     cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
     func(n);
     cout << "라고 답변하였지.\n";
+    return 0;
 }
