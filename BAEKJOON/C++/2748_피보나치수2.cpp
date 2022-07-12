18a619a62049fed7230b9fbcded2c9336c49435c
wlghd5524 <wlghd5524@gmail.com>
2022-07-12 16:38:58 +0900

    Bronze 1

--- BAEKJOON/C++/2748_피보나치수2.cpp
+++ BAEKJOON/C++/2748_피보나치수2.cpp
@@ -1,14 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-    int n,p=0;
+int main(){
+    int n, p = 0;
     cin >> n;
-    long long int arr[n];
+    unsigned long long int arr[n];
     arr[0] = 0;
     arr[1] = 1;
-    for(int i=2;i<n+1;i++) {
-        arr[i] = arr[i-1] + arr[i-2];
-    }
+    for (int i = 2; i < n + 1; i++)
+        arr[i] = arr[i - 1] + arr[i - 2];
     cout << arr[n];
     return 0;
 }
