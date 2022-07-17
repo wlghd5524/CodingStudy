e394aa46c88e422b8562fcfe4330f83fe7ee6c06
wlghd5524 <wlghd5524@gmail.com>
2022-07-17 22:24:42 +0900

    Bronze 1

--- BAEKJOON/C++/1259_팰린드롬수.cpp
+++ BAEKJOON/C++/1259_팰린드롬수.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void printYes() {
+    cout << "yes\n";
+}
+void printNo() {
+    cout << "no\n";
+}
 int main() {
     string str;
     while (1) {
@@ -8,30 +14,30 @@ int main() {
         if (str == "0")
             break;
         if (str.size() == 1)
-            cout << "yes\n";
+            printYes();
         else if (str.size() == 2) {
             if (str[0] == str[1])
-                cout << "yes\n";
+                printYes();
             else
-                cout << "no\n";
+                printNo();
         }
         else if (str.size() == 3) {
             if (str[0] == str[2])
-                cout << "yes\n";
+                printYes();
             else
-                cout << "no\n";
+                printNo();
         }
         else if (str.size() == 4) {
             if (str[0] == str[3] && str[1] == str[2])
-                cout << "yes\n";
+                printYes();
             else
-                cout << "no\n";
+                printNo();
         }
         else if (str.size() == 5) {
             if (str[0] == str[4] && str[1] == str[3])
-                cout << "yes\n";
+                printYes();
             else
-                cout << "no\n";
+                printNo();
         }
     }
     return 0;
