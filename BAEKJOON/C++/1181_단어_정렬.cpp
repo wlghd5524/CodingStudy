989eb2d0e196c8c635de4fbcd87b6508fe32ef46
wlghd5524 <wlghd5524@gmail.com>
2022-07-20 17:28:47 +0900

    Silver 5

--- BAEKJOON/C++/1181_단어_정렬.cpp
+++ BAEKJOON/C++/1181_단어_정렬.cpp
@@ -4,28 +4,24 @@
 #include <vector>
 using namespace std;
 bool cmp(string a, string b) {
-    if(a.size()==b.size()) {
-        return a<b;
-    }
-    return a.size()<b.size();
+    if (a.size() == b.size())
+        return a < b;
+    return a.size() < b.size();
 }
 int main() {
     int n, count = 0;
     cin >> n;
-    //string word[n];
     vector<string> word;
     string tmp;
-    for(int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++) {
         cin >> tmp;
         word.push_back(tmp);
     }
-    sort(word.begin(),word.end(),cmp);
-    for(int i=0;i<n;i++) {
-        if(word[i]==word[i+1]&&i+1<n) {
-            word.erase(word.begin()+i-1);
-        }
+    sort(word.begin(), word.end(), cmp);
+    for (int i = 0; i < n; i++) {
+        if (word[i] == word[i + 1] && i + 1 < n)
+            word.erase(word.begin() + i - 1);
     }
-    for(int i=0;i<word.size();i++) {
+    for (int i = 0; i < word.size(); i++)
         cout << word[i] << "\n";
-    }
 }
