6fc751d02ef9282b783a515d00883ca5de823aa5
wlghd5524 <wlghd5524@gmail.com>
2022-07-21 11:23:56 +0900

    Silver 5

---
+++ BAEKJOON/C++/1085_직사각형에서_탈출.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,w,h;
+    cin >> x>> y>>w>>h;
+    for(int i=x;i<=w;i++) {
+        for(int j=y;j<=h;j++) {
+            if(i==w||j==h) {
+                
+            }
+        }
+    }
+}
