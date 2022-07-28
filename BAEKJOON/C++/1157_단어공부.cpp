a8b65d30cbe457aed9ff96eb69bcf2677f8f83cf
wlghd5524 <wlghd5524@gmail.com>
2022-07-28 10:52:31 +0900

    Bronze 1

--- BAEKJOON/C++/1157_단어공부.cpp
+++ BAEKJOON/C++/1157_단어공부.cpp
@@ -3,8 +3,8 @@
 #include <cctype>
 using namespace std;
 int main() {
-    int maxCount = 0;
-    int maxIndex = 0;
+    int max_count = 0;
+    int max_index = 0;
     int i;
     string str;
     cin >> str;
@@ -16,17 +16,17 @@ int main() {
             count[str[i] - 97]++;
     }
     for (i = 0; i < 26; i++) {
-        if (maxCount < count[i]) {
-            maxIndex = i;
-            maxCount = count[i];
+        if (max_count < count[i]) {
+            max_index = i;
+            max_count = count[i];
         }
     }
     for (i = 0; i < 26; i++) {
-        if (count[i] == maxCount && i != maxIndex) {
+        if (count[i] == max_count && i != max_index) {
             cout << "?";
             return 0;
         }
     }
-    cout << char(maxIndex + 65);
+    cout << char(max_index + 65);
     return 0;
 }
