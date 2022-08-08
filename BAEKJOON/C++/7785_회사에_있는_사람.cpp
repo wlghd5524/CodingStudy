c4ba73b0938a9d814d4b677a081fb54f3e82c77e
wlghd5524 <wlghd5524@gmail.com>
2022-08-08 10:08:25 +0900

    Silver 5

--- BAEKJOON/C++/7785_회사에_있는_사람.cpp
+++ BAEKJOON/C++/7785_회사에_있는_사람.cpp
@@ -7,25 +7,9 @@ int main() {
     map<string, string, greater<string>> m;
     int n;
     cin >> n;
-    cin.ignore();
     for (int i = 0; i < n; i++) {
-        getline(cin, str);
-        for (int j = 0; j < str.size(); j++) {
-            if (str[j] != ' ') {
-                tmp += str[j];
-            }
-            else {
-                name = tmp;
-                m[name] = "";
-                tmp.clear();
-            }
-            if (j + 1 == str.size()) {
-                log = tmp;
-                m[name] = log;
-                log.clear();
-                tmp.clear();
-            }
-        }
+        cin >> name >> log;
+        m[name] = log;
     }
     int j = 0;
     for (auto iter = m.begin(); iter != m.end(); iter++) {
