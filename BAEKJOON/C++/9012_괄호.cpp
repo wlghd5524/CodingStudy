9ebc77246d8ba04e9372b4c712ea344e40edb85f
wlghd5524 <wlghd5524@gmail.com>
2022-08-15 20:46:52 +0900

    Silver 4

--- BAEKJOON/C++/9012_괄호.cpp
+++ BAEKJOON/C++/9012_괄호.cpp
@@ -2,30 +2,31 @@
 using namespace std;
 
 int main() {
-    int t, leftCount = 0, rightCount = 0;
+    int t, left_count = 0, right_count = 0;
     cin >> t;
     string str;
     for (int i = 0; i < t; i++) {
         cin >> str;
-        leftCount = 0;
-        rightCount = 0;
+        left_count = 0;
+        right_count = 0;
         if (str.size() % 2 == 1) {
             cout << "NO\n";
             continue;
         }
         for (int j = 0; j < str.size(); j++) {
             if (str[j] == '(')
-                leftCount++;
+                left_count++;
             else if (str[j] == ')') {
-                rightCount++;
-                if (leftCount < rightCount)
+                right_count++;
+                if (left_count < right_count)
                     break;
             }
         }
-        if (leftCount != rightCount)
+        if (left_count != right_count)
             cout << "NO\n";
         else {
             cout << "YES\n";
         }
     }
+    return 0;
 }
