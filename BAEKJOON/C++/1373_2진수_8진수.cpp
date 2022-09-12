d0da8971fd0538c88c095af6d11189ff89e1f7c1
wlghd5524 <wlghd5524@gmail.com>
2022-09-12 19:39:21 +0900

    Bronze 1

---
+++ BAEKJOON/C++/1373_2진수_8진수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    int count = 0, sum = 0;
+    string str,result;
+    cin >> str;
+    for(int i=str.size()-1;i>=0;i--) {
+        if(str[i]=='1') {
+            sum += pow(2,count);
+        }
+        if(count == 2 || i == 0) {
+            result.push_back(sum + '0');
+            count = 0;
+            sum = 0;
+            continue;
+        }
+
+        count++;
+    }
+    reverse(result.begin(),result.end());
+    cout << result;
+}
