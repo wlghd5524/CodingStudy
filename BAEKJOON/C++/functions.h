4e227f9842455b237c8b689a1744940dc975c9a8
wlghd5524 <wlghd5524@gmail.com>
2022-09-20 16:02:11 +0900

    Decrease time complexity Add and Multi

--- BAEKJOON/C++/functions.h
+++ BAEKJOON/C++/functions.h
@@ -12,28 +12,27 @@ string bigNumAdd(string x, string y) {
         x = y;
         y = tmp;
     }
-    int x_leng = x.size();
-    for(int i = y.size();i<x.size();i++) {
-        y.insert(0,"0");
+    for(int i = y.size(); i < x.size(); i++) {
+        y = "0" + y;
     }
-    reverse(x.begin(),x.end());
-    reverse(y.begin(),y.end());
-    for(int i = 0; i < x_leng; i++) {
+    for(int i = x.size()-1; i >= 0; i--) {
         int tmp = (x[i]-'0') + (y[i]-'0');
         if(tmp >= 10) {
             tmp -= 10;
-            if(i+1==x.size()) {
-                x.push_back('1');
+            if(i == 0) {
+                x = "1" + x;
+                x[i+1] = tmp + '0';
+                break;
             }
             else {
-                x[i+1]++;
+                x[i-1]++;
             }
         }
         x[i] = tmp + '0';
     }
-    reverse(x.begin(),x.end());
     return x;
 }
+
 
 string bigNumSub(string x, string y) {
     if(x.size()<y.size()) {
@@ -50,26 +49,24 @@ string bigNumSub(string x, string y) {
             y = tmp;
         }
     }
-    int x_leng = x.size();
-    for(int i = y.size();i<x.size();i++) {
-        y.insert(0,"0");
+    for(int i = y.size(); i < x.size(); i++) {
+        y = "0" + y;
     }
-    reverse(x.begin(),x.end());
-    reverse(y.begin(),y.end());
-    for(int i = 0; i < x_leng; i++) {
+    for (int i = x.size()-1; i >= 0 ; i--) {
         int tmp = (x[i]-'0') - (y[i]-'0');
         if(tmp < 0) {
             tmp += 10;
-            x[i+1]--;
+            x[i-1]--;
         }
         x[i] = tmp + '0';
     }
-    reverse(x.begin(),x.end());
-    while(x.find("0")!=string::npos) {
-        x.erase(x.find("0"),1);
+    while(*x.begin()=='0') {
+        x.erase(0,1);
     }
     return x;
 }
+
+
 string bigNumMulti(string x, string y) {
     string sum;
     if(x.size()<y.size()) {
@@ -86,31 +83,25 @@ string bigNumMulti(string x, string y) {
             y = tmp;
         }
     }
-    int x_leng = x.size();
-    int y_leng = y.size();
-    reverse(x.begin(),x.end());
-    reverse(y.begin(),y.end());
     int tmp = 0;
-    for(int i = 0; i < y_leng; i++) {
+    for(int i = y.size()-1; i >= 0; i--) {
         string result;
         tmp = 0;
-        for(int j = 0; j < x_leng; j++) {
+        for(int j = x.size()-1; j >= 0; j--) {
             int a = (x[j]-'0') * (y[i]-'0') + tmp;
             tmp = 0;
             if(a >= 10) {
                 tmp = (a/10);
-                result.push_back((a%10)+'0');
+                result = to_string((a%10)) + result;
             }
             else {
-                result.push_back(a+'0');
+                result = to_string(a) + result;
             }
-            if(j+1==x_leng && tmp!=0) {
-                result.push_back(tmp+'0');
+            if(j==0 && tmp!=0) {
+                result = to_string(tmp) + result;
             } 
-            
         }
-        reverse(result.begin(),result.end());
-        for(int p=0;p<i;p++) {
+        for(int p=0;p<y.size()-1-i;p++) {
             result.push_back('0');
         }
         sum = bigNumAdd(sum,result);
