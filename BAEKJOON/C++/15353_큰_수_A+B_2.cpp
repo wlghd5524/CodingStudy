945b0fc118764ae69ada58011c77713e1a72850c
wlghd5524 <wlghd5524@gmail.com>
2022-10-26 17:03:21 +0900

    Silver 5

--- BAEKJOON/C++/15353_큰_수_A+B_2.cpp
+++ BAEKJOON/C++/15353_큰_수_A+B_2.cpp
@@ -1,51 +1,35 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
-int main() {
-    string a, b, tmp;
-    cin >> a >> b;
-    reverse(a.begin(), a.end());
-    reverse(b.begin(), b.end());
-    int min, j = 0;
-    if (a.size() < b.size()) {
-        tmp = b;
-        b = a;
-        a = tmp;
+string bigNumAdd(string x, string y) {
+    if(x.size() < y.size()) {
+        string tmp;
+        tmp = x;
+        x = y;
+        y = tmp;
     }
-    a.append("0");
-    for (int i = 0; i < b.size(); i++) {
-        int x = a[i] - '0';
-        int y = b[i] - '0';
-        int z = a[i + 1] - '0';
-
-        if (x >= 10) {
-            x %= 10;
-            a[i + 1] = (z + (x / 10)) + '0';
-        }
-        if (x + y >= 10) {
-            a[i] = ((x + y) % 10) + '0';
-            if (i + 1 == a.size()) {
-                a.append(to_string((x + y) / 10));
+    for(int i = y.size();i<x.size();i++) {
+        y.insert(0,"0");
+    }
+    for(int i = x.size()-1; i >= 0; i--) {
+        int tmp = (x[i]-'0') + (y[i]-'0');
+        if(tmp >= 10) {
+            tmp -= 10;
+            if(i==0) {
+                x.insert(0,"1");
+                x[i+1] = tmp + '0';
+                break;
             }
             else {
-                a[i + 1] = (z + ((x + y) / 10)) + '0';
-                j = i;
-                while (a[j + 1] >= 10 + '0') {
-                    int p = a[j + 1] - '0';
-                    a[j + 1] = ((a[j + 1] - '0') % 10) + '0';
-                    a[j + 2] = (a[j + 2] - '0' + (p / 10)) + '0';
-                    j++;
-                }
+                x[i-1]++;
             }
         }
-        else {
-            a[i] = x + y + '0';
-        }
+        x[i] = tmp + '0';
     }
-    reverse(a.begin(), a.end());
-    if (a.front() == '0') {
-        a.erase(0, 1);
-    }
-    cout << a;
+    return x;
+}
+int main() {
+    string a,b;
+    cin >> a >> b;
+    cout << bigNumAdd(a,b);
+    return 0;
 }
