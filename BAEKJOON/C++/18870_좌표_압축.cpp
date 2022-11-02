3e0b4461acfdd4c3c96f0994421eca58c7dab9a4
wlghd5524 <wlghd5524@gmail.com>
2022-11-02 17:35:25 +0900

    Silver 2

--- BAEKJOON/C++/18870_좌표_압축.cpp
+++ BAEKJOON/C++/18870_좌표_압축.cpp
@@ -9,19 +9,19 @@ int main() {
     int n, data;
     cin >> n;
     vector<int> v;
-    map<int, int> m;
+    map<int, int> points;
     for (int i = 0; i < n; i++) {
         cin >> data;
         v.emplace_back(data);
-        m[data] = 0;
+        points[data] = 0;
     }
     int cnt = 0;
-    for (auto iter = m.begin(); iter != m.end(); iter++) {
+    for (auto iter = points.begin(); iter != points.end(); iter++) {
         iter->second = cnt;
         cnt++;
     }
     for (int i = 0; i < v.size(); i++) {
-        cout << m[v[i]] << " ";
+        cout << points[v[i]] << " ";
     }
     return 0;
 }
