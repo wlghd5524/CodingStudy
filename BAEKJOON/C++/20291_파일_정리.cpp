a1ce6fc2fa0ba7104979fac58e5924b5ffee9e83
wlghd5524 <wlghd5524@gmail.com>
2022-11-02 17:37:15 +0900

    Silver 3

--- BAEKJOON/C++/20291_파일_정리.cpp
+++ BAEKJOON/C++/20291_파일_정리.cpp
@@ -11,16 +11,16 @@ int main() {
     string extension;
     int n;
     cin >> n;
-    map<string, int> m;
+    map<string, int> extension_list;
     for (int i = 0; i < n; i++) {
         cin >> file_name;
         for (j = file_name.size() - 1; file_name[j] != '.'; j--){}
         for (j += 1; j < file_name.size(); j++)
             extension += file_name[j];
-        m[extension]++;
+        extension_list[extension]++;
         extension.clear();
     }
-    for (auto iter = m.begin(); iter != m.end(); iter++)
+    for (auto iter = extension_list.begin(); iter != extension_list.end(); iter++)
         cout << iter->first << " " << iter->second << "\n";
     return 0;
 }
