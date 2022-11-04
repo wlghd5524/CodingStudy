ee794522e46670da4e636c86a0fb335e397f4e43
wlghd5524 <wlghd5524@gmail.com>
2022-11-04 16:43:59 +0900

    Silver 4

--- BAEKJOON/C++/9012_괄호.cpp
+++ BAEKJOON/C++/9012_괄호.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
+#include <stack>
+
 using namespace std;
 
-int main() {
-    int t, left_count = 0, right_count = 0;
+bool solution(string s);
+
+int main()
+{
+    int t;
     cin >> t;
     string str;
-    for (int i = 0; i < t; i++) {
+    for (int i = 0; i < t; i++)
+    {
+        bool is_correct = true;
         cin >> str;
-        left_count = 0;
-        right_count = 0;
-        if (str.size() % 2 == 1) {
-            cout << "NO\n";
-            continue;
-        }
-        for (int j = 0; j < str.size(); j++) {
-            if (str[j] == '(')
-                left_count++;
-            else if (str[j] == ')') {
-                right_count++;
-                if (left_count < right_count)
-                    break;
-            }
-        }
-        if (left_count != right_count)
-            cout << "NO\n";
-        else {
+        is_correct = solution(str);
+        if (is_correct)
+        {
             cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
         }
     }
     return 0;
+}
+bool solution(string s)
+{
+    stack<char> st;
+    bool answer = true;
+
+    if (s.size() % 2 != 0)
+    {
+        answer = false;
+    }
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (st.empty())
+            {
+                answer = false;
+                break;
+            }
+            st.pop();
+        }
+    }
+    if (!st.empty())
+    {
+        answer = false;
+    }
+    return answer;
 }
