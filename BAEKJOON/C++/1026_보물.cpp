7df754dd24859ded85dcf969593709ed1c1fcf52
wlghd5524 <wlghd5524@gmail.com>
2022-11-12 15:12:03 +0900

    Silver 4

--- BAEKJOON/C++/1026_보물.cpp
+++ BAEKJOON/C++/1026_보물.cpp
@@ -2,19 +2,28 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main() {
+int main()
+{
     int n, sum = 0;
     cin >> n;
     vector<int> a(n);
     vector<int> b(n);
     for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
+    }
+
     for (int i = 0; i < n; i++)
+    {
         cin >> b[i];
+    }
+
     sort(a.begin(), a.end());
     sort(b.begin(), b.end(), greater<>());
     for (int i = 0; i < n; i++)
+    {
         sum += (a[i] * b[i]);
+    }
     cout << sum;
     return 0;
 }
