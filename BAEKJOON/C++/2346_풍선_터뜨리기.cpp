7011705d7dc44bc32d29dee6ec33ec8f4cdb904c
wlghd5524 <wlghd5524@gmail.com>
2022-11-16 14:34:08 +0900

    Silver 3

--- BAEKJOON/C++/2346_풍선_터뜨리기.cpp
+++ BAEKJOON/C++/2346_풍선_터뜨리기.cpp
@@ -2,48 +2,51 @@
 #include <deque>
 #include <vector>
 using namespace std;
-int main() {
-    deque<pair<int,int>> dq;
+int main()
+{
+    deque<pair<int, int>> dq;
     vector<int> result;
     int n;
     cin >> n;
-    for(int i=0;i<n;i++) {
-        pair<int,int> tmp;
+    for (int i = 0; i < n; i++)
+    {
+        pair<int, int> tmp;
         cin >> tmp.first;
-        dq.push_back({tmp.first,i+1});
+        dq.push_back({tmp.first, i + 1});
     }
     int cur = dq.front().first;
     result.emplace_back(dq.front().second);
     dq.pop_front();
-    while(!dq.empty()) {
-        //int dq_front = dq.front().first;
-        //int dq_back = dq.back().first;
-        
-        for(int i=0;i<abs(cur)-1;i++) {
-            
-            if(cur > 0) {
+    while (!dq.empty())
+    {
+        for (int i = 0; i < abs(cur) - 1; i++)
+        {
+            if (cur > 0)
+            {
                 dq.push_back(dq.front());
                 dq.pop_front();
-                
             }
-            else {
+            else
+            {
                 dq.push_front(dq.back());
                 dq.pop_back();
             }
         }
-        if(cur > 0) {
+        if (cur > 0)
+        {
             cur = dq.front().first;
             result.emplace_back(dq.front().second);
             dq.pop_front();
         }
-        else {
+        else
+        {
             cur = dq.back().first;
             result.emplace_back(dq.back().second);
             dq.pop_back();
         }
-
     }
-    for(int i=0;i<result.size();i++) {
+    for (int i = 0; i < result.size(); i++)
+    {
         cout << result[i] << " ";
     }
     return 0;
