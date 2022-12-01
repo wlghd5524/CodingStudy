3eb8c1f7d2f661af03cbff53a93d7da43ce4ac1c
wlghd5524 <wlghd5524@gmail.com>
2022-12-01 14:41:45 +0900

    Bronze 3

---
+++ BAEKJOON/C++/2501_약수_구하기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cnt++;
+        }
+        if (cnt == k)
+        {
+            cout << i;
+            return 0;
+        }
+    }
+    if (cnt < k)
+    {
+        cout << 0;
+    }
+    return 0;
+}
