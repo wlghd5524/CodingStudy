658cb3433f922d332d90892a08f3910de4a6f22b
wlghd5524 <wlghd5524@gmail.com>
2022-12-10 21:08:43 +0900

    Bronze 2

--- BAEKJOON/C++/1152_단어의_개수.cpp
+++ BAEKJOON/C++/1152_단어의_개수.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
+int main()
+{
     int count = 0;
     string str;
     getline(cin, str);
-    for (int i = 0; i < str.size(); i++) {
-        if (str == " ") {
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str == " ")
+        {
             cout << 0;
             return 0;
         }
-        if (str[i] == ' ') {
+        if (str[i] == ' ')
+        {
             if (i != 0 && i != str.size() - 1)
                 count++;
         }
