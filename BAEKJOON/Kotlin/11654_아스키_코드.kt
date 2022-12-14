0c742dbd0f28bf569a970d53b4648d4c6391db4c
wlghd5524 <wlghd5524@gmail.com>
2022-12-14 10:41:51 +0900

    Bronze 5

---
+++ BAEKJOON/Kotlin/11654_아스키_코드.kt
@@ -0,0 +1,7 @@
+import java.io.*
+import java.util.*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var n = StringTokenizer(br.readLine()).nextToken()[0]
+    print(n.toInt())
+}
