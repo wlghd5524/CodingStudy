af79aff9b2d987bba093a01fcf8d8a5ac08dd7e0
wlghd5524 <wlghd5524@gmail.com>
2022-12-19 11:27:57 +0900

    Bronze 3

---
+++ BAEKJOON/Kotlin/3053_택시_기하학.kt
@@ -0,0 +1,8 @@
+import java.util.*
+import kotlin.math.*
+fun main() {
+    val input = Scanner(System.`in`)
+    var r:Double = input.nextDouble()
+    println(String.format("%.6f",r*r*PI))
+    println(String.format("%.6f",r*r*2))
+}
