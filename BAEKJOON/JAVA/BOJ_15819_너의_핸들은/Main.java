b09ba49a5f7bb047d63c80300f65e7d7648f08bd
wlghd5524 <wlghd5524@gmail.com>
2022-12-31 22:23:55 +0900

    Bronze 1

--- BAEKJOON/JAVA/BOJ_15819_너의_핸들은/Main.java
+++ BAEKJOON/JAVA/BOJ_15819_너의_핸들은/Main.java
@@ -1,23 +1,24 @@
 package BOJ_15819_너의_핸들은;
+
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int k = input.nextInt();
         TreeSet<String> handles = new TreeSet<String>();
-        for(int i = 0;i<n;i++) {
+        for (int i = 0; i < n; i++) {
             String tmp = input.next();
             handles.add(tmp);
         }
         Iterator<String> iter = handles.iterator();
         int cnt = 1;
-        while(iter.hasNext()) {
-            if(cnt==k) {
+        while (iter.hasNext()) {
+            if (cnt == k) {
                 System.out.print(iter.next());
                 break;
-            }
-            else {
+            } else {
                 iter.next();
             }
             cnt++;
