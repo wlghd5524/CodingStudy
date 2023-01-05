43f8225d75b2558cb72f53dd62cc3224314c36cc
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-01-05 15:43:33 +0900

    [Silver III] Title: 참외밭, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -10,45 +10,43 @@ public class Main {
         int y_max = 0;
         int y_max_index = 0;
         int[] arr = new int[6];
-        for(int i = 0;i < 6;i++) {
+        for (int i = 0; i < 6; i++) {
             int direction = sc.nextInt();
             arr[i] = sc.nextInt();
-            if(direction == 1 || direction == 2) {
-                if(arr[i] > x_max) {
+            if (direction == 1 || direction == 2) {
+                if (arr[i] > x_max) {
                     x_max = arr[i];
                     x_max_index = i;
                 }
-            }
-            else if(direction == 3 || direction == 4) {
-                if(arr[i] > y_max) {
+            } else if (direction == 3 || direction == 4) {
+                if (arr[i] > y_max) {
                     y_max = arr[i];
                     y_max_index = i;
                 }
             }
         }
-        int tmp_x;
-        int tmp_y;
-        if(x_max_index==0) {
-            tmp_x = Math.max(arr[x_max_index+1], arr[5]) - Math.min(arr[x_max_index+1], arr[5]); 
+        int tmp_x = 0;
+        int tmp_y = 0;
+        if (x_max_index == 0) {
+            tmp_x = sub(arr[x_max_index + 1], arr[5]);
+        } else if (x_max_index == 5) {
+            tmp_x = sub(arr[x_max_index - 1], arr[0]);
+        } else {
+            tmp_x = sub(arr[x_max_index - 1], arr[x_max_index + 1]);
         }
-        else if(x_max_index == 5) {
-            tmp_x = Math.max(arr[x_max_index-1], arr[0]) - Math.min(arr[x_max_index-1], arr[0]);
+        if (y_max_index == 0) {
+            tmp_y = sub(arr[y_max_index + 1], arr[5]);
+        } else if (y_max_index == 5) {
+            tmp_y = sub(arr[y_max_index - 1], arr[0]);
+        } else {
+            tmp_y = sub(arr[y_max_index - 1], arr[y_max_index + 1]);
         }
-        else {
-            tmp_x = Math.max(arr[x_max_index-1], arr[x_max_index+1]) - Math.min(arr[x_max_index-1], arr[x_max_index+1]);
-        }
-        if(y_max_index==0) {
-            tmp_y = Math.max(arr[y_max_index+1], arr[5]) - Math.min(arr[y_max_index+1], arr[5]); 
-        }
-        else if(y_max_index == 5) {
-            tmp_y = Math.max(arr[y_max_index-1], arr[0]) - Math.min(arr[y_max_index-1], arr[0]);
-        }
-        else {
-            tmp_y = Math.max(arr[y_max_index-1], arr[y_max_index+1]) - Math.min(arr[y_max_index-1], arr[y_max_index+1]);
-        }
-
-        area = (x_max * y_max) - (tmp_x*tmp_y);
-        System.out.println(area*k);
+        area = (x_max * y_max) - (tmp_x * tmp_y);
+        System.out.println(area * k);
         sc.close();
+    }
+
+    public static int sub(int a, int b) {
+        return Math.max(a, b) - Math.min(a, b);
     }
 }
