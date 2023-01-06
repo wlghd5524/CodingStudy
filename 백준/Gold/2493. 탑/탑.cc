5455aa317acb91ad0006e9fc86b9cdd0c79676c8
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-01-06 14:49:05 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,tmp;
+    cin >> n;
+    stack<pair<int,int>> s;
+    for(int i=0;i<n;i++) {
+        cin >> tmp;
+        while(s.empty()==0) {
+            if(s.top().second>tmp) {
+                cout << s.top().first << " ";
+                break;
+            }
+            s.pop();
+        }
+        if(s.empty()==1) {
+            cout << 0 << " ";
+        }
+        s.push({i+1,tmp});
+    }
+    return 0;
+
+}
