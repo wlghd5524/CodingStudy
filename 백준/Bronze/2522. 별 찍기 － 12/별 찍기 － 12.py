ef36f019442d4577174711d34e0e28d0109ea423
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-01-16 09:58:57 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 52 ms, Memory: 30620 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(0,n):
+    for j in range(0,n-1-i):
+        print(" ",end="")
+    for j in range(0,i+1):
+        print("*",end="")
+    print("")
+for i in range(0,n-1):
+    for j in range (0,i+1):
+        print(" ",end="")
+    for j in range (0,n-1-i):
+        print("*",end ="")
+    print("")
