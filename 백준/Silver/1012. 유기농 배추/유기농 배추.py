7a9a6f4f5017d2b503a94cfbde9e8d3e4e01f9db
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-02-02 17:14:47 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31556 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000000)
+def dfs(a, b):
+    graph[a][b] = 0
+    if b-1 >= 0 and graph[a][b-1] == 1:
+        dfs(a,b-1)
+    if b+1 < m and graph[a][b+1] == 1:
+        dfs(a,b+1)
+    if a-1 >= 0 and graph[a-1][b] == 1:
+        dfs(a-1,b)
+    if a+1 < n and graph[a+1][b] == 1:
+        dfs(a+1,b)
+    
+T = int(input())
+for _ in range(T):
+    m, n, k = map(int, input().split())
+    graph = [[0 for i in range(m)] for j in range(n)]
+    for i in range(k):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
