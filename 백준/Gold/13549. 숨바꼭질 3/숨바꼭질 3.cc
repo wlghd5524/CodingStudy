95ee6105702e1199dd7685aafe94d0e66b549d2d
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-03-03 13:50:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 4 ms, Memory: 6784 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int bfs(int v, int k)
+{
+    bool visited[1000001] = {
+        false,
+    };
+    int path[1000001] = {
+        0,
+    };
+    queue<int> q;
+    visited[v] = true;
+    q.push(v);
+    while (!q.empty())
+    {
+        int x = q.front();
+        if (x == k)
+        {
+            break;
+        }
+        q.pop();
+        if (visited[x * 2] == false && x * 2 >= 0 && x * 2 < 100001)
+        {
+            visited[x * 2] = true;
+            q.push(x * 2);
+            path[x * 2] = path[x];
+        }
+        if (visited[x - 1] == false && x - 1 >= 0 && x - 1 < 100001)
+        {
+            visited[x - 1] = true;
+            q.push(x - 1);
+            path[x - 1] = path[x] + 1;
+        }
+        if (visited[x + 1] == false && x + 1 >= 0 && x + 1 < 100001)
+        {
+            visited[x + 1] = true;
+            q.push(x + 1);
+            path[x + 1] = path[x] + 1;
+        }
+        
+        
+    }
+    return path[k];
+}
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    cout << bfs(N, K);
+    return 0;
+}
