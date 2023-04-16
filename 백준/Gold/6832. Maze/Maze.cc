496a780307a17d3947429f57d5a49df13319febd
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-04-16 19:21:14 +0900

    [Gold V] Title: Maze, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/6832. Maze/Maze.cc
+++ 백준/Gold/6832. Maze/Maze.cc
@@ -9,64 +9,6 @@ int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 int result = 0;
 int dist[20][20];
-void dfs(int x, int y, int cnt)
-{
-    visited[x][y] = true;
-    if (x == n - 1 && y == m - 1)
-    {
-        result = cnt;
-        return;
-    }
-    if (graph[x][y] == '+')
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            int xx = x + dx[i];
-            int yy = y + dy[i];
-            if (xx >= 0 && xx < n && yy >= 0 && yy < m)
-            {
-                if (!visited[xx][yy] && graph[xx][yy] != '*')
-                {
-                    dfs(xx, yy, cnt + 1);
-                }
-            }
-        }
-    }
-    if (graph[x][y] == '-')
-    {
-        if (y - 1 >= 0)
-        {
-            if (!visited[x][y - 1] && graph[x][y - 1] != '*')
-            {
-                dfs(x, y - 1, cnt + 1);
-            }
-        }
-        if (y + 1 < m)
-        {
-            if (!visited[x][y + 1] && graph[x][y + 1] != '*')
-            {
-                dfs(x, y + 1, cnt + 1);
-            }
-        }
-    }
-    if (graph[x][y] == '|')
-    {
-        if (x - 1 >= 0)
-        {
-            if (!visited[x - 1][y] && graph[x - 1][y] != '*')
-            {
-                dfs(x - 1, y, cnt + 1);
-            }
-        }
-        if (x + 1 < n)
-        {
-            if (!visited[x + 1][y] && graph[x + 1][y] != '*')
-            {
-                dfs(x + 1, y, cnt + 1);
-            }
-        }
-    }
-}
 void bfs() {
     queue<pair<int,int>> q;
     q.push({0,0});
