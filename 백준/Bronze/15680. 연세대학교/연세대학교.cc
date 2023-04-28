dbae29487b047e45f73a34934cc8ee13a556e5af
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-04-28 19:35:53 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 0;
+    cin >> n;
+    if(n == 0) {
+        cout << "YONSEI";
+    }
+    else {
+        cout << "Leading the Way to the Future";
+    }
+    
+    return 0;
+}
