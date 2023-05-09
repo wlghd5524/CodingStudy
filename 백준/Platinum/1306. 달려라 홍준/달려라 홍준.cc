ebce00df534ffb4510ad3e835e06c896be8f6000
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-05-09 09:50:57 +0900

    [Platinum V] Title: 달려라 홍준, Time: 392 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Platinum/1306. 달려라 홍준/달려라 홍준.cc
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.cc
@@ -28,6 +28,9 @@ int result(int node, int start, int end, int left, int right)
 }
 int main()
 {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
