9f083246fc307e9d5666f85435d426eed018a09b
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-05-27 23:18:51 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if(100*a >= b) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
