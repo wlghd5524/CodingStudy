9d222822901b97ac4599d2cd94ac42d09af9182a
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-05-28 22:58:38 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    if(s == "NLCS") {
+        cout << "North London Collegiate School";
+    }
+    else if(s == "BHA") {
+        cout << "Branksome Hall Asia";
+    }
+    else if(s == "KIS") {
+        cout << "Korea International School";
+    }
+    else if(s == "SJA") {
+        cout << "St. Johnsbury Academy";
+    }
+    return 0;
+}
