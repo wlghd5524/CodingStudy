fc54769f22eb8ab5226e3b95197d3b227d177f4d
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2023-10-02 14:01:18 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 8 ms, Memory: 5928 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> dp(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2;i<=n;i++) {
+        dp[i] = ((dp[i-1] % 1000000007) + (dp[i-2] % 1000000007)) % 1000000007;
+    }
+    cout << dp[n];
+}
