a04354d8f57ba8adecca1fd3c663425481464248
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-03-02 16:44:47 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 220 ms, Memory: 9828 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N, M, R;
+vector<vector<int>> graph;
+vector<int> isVisited;
+vector<int> order;
+
+void bfs(int start)
+{
+    queue<int> q;
+    q.push(start);
+    isVisited[start] = true;
+    int count = 1;
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        order[cur] = count;
+        count++;
+        for (int i = 0; i < graph[cur].size(); i++)
+        {
+            int next = graph[cur][i];
+            if (!isVisited[next])
+            {
+                q.push(next);
+                isVisited[next] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M >> R;
+    graph.resize(N + 1);
+    isVisited.resize(N + 1);
+    order.resize(N + 1);
+    for (int i = 0; i < M; i++)
+    {
+        int from, to;
+        cin >> from >> to;
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        sort(graph[i].begin(), graph[i].end());
+    }
+    bfs(R);
+    for (int i = 1; i <= N; i++)
+    {
+        cout << order[i] << "\n";
+    }
+    return 0;
+}
