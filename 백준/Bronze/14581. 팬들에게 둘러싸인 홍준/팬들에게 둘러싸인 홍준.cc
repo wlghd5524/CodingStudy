8ad89333a06b02ef9c24cf1d135a6a6ed84a6591
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-03-08 18:19:41 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cin >> name;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << name << "::fan:\n";
+    cout << ":fan::fan::fan:";
+    return 0;
+}
