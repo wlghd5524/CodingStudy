1957c46af1cd2dedb541dbb8fcfb7d1f6563ed8a
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-03-22 19:31:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2688 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int N = 0;
+    int number[10001] = {
+        0,
+    };
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        number[num]++;
+    }
+    for (int i = 1; i <= 10000; i++)
+    {
+        for (int j = 0; j < number[i]; j++)
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
