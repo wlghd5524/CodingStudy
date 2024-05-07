7cfb0b6db73785298eea5047689ca328c84f4dbd
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-05-07 15:44:25 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 1112 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int min = 1001;
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        if (a < b && min > b)
+        {
+            min = b;
+        }
+    }
+    if(min == 1001) {
+        printf("-1");
+    }
+    else {
+        printf("%d", min);
+    }
+    return 0;
+}
