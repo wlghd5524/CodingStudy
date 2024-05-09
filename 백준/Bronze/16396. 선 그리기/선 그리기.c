13785815bb3b183859262c147c57153e9f48e080
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-05-09 09:02:50 +0900

    [Bronze I] Title: 선 그리기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16396. 선 그리기/선 그리기.c
+++ 백준/Bronze/16396. 선 그리기/선 그리기.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int line[10001] = {
-        0,
-    };
+    int line[10001] = {0,}; // 줄이 그어져 있는 부분. line[1] = 1이면 좌표 1~2구간에 줄이 그어져 있다는 것
     int result = 0;
     int n = 0;
     scanf("%d", &n);
@@ -20,27 +18,10 @@ int main()
     int endPoint = -1;
     for (int i = 1; i <= 10000; i++)
     {
-        if(line[i] == 1) {
+        if (line[i] == 1)
+        {
             result++;
         }
-
-
-
-
-        // if (line[i] == 1 && line[i - 1] != 1)
-        // {
-        //     startPoint = i;
-        // }
-        // if ((line[i] == 0 && line[i - 1] == 1))
-        // {
-        //     endPoint = i - 1;
-        //     result += (endPoint - startPoint);
-        // }
-        // if (i == 10000 && line[i] == 1)
-        // {
-        //     endPoint = i;
-        //     result += (endPoint - startPoint);
-        // }
     }
     printf("%d", result);
     return 0;
