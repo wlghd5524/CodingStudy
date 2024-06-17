95a680305aa0922955b4f0d1b71a9c52f47b8ca6
JihongMin <42297867+wlghd5524@users.noreply.github.com>
2024-06-17 09:36:19 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    map<string,int> m;
+    for(int i = 0; i<words.size();i++) {
+        m[words[i]] = 0;
+    }
+    queue<pair<string,int>> q;
+    q.push({begin,0});
+    while(!q.empty()) {
+        string cur = q.front().first;
+        int dis = q.front().second;
+        q.pop();
+        if(cur == target) {
+            answer = dis;
+            break;
+        }
+        for(int i = 0;i<cur.size();i++) {
+            string temp = cur;
+            for(int j = 'a';j<'z';j++) {
+                temp[i] = j;
+                if(m.find(temp) != m.end() && m[temp] == 0) {
+                    q.push({temp,dis+1});
+                    m[temp] = dis + 1;
+                }
+            }
+        }
+        
+    }
+    return answer;
+}
